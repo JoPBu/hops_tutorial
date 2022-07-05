30f75e10bdf0f25ed7bb904f5c224543292ab743
JoPBu <76694685+JoPBu@users.noreply.github.com>
2022-07-05 15:02:55 +0200

    Finished the tutorial

---
+++ Gh_Compute_Sample.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import os
+import sys
+import compute_rhino3d.Util as rcutil
+import compute_rhino3d.Grasshopper as rcgh
+
+
+# Set the compute server connection
+compute_url = "http://localhost:5000/"
+post_url = compute_url + "grasshopper"
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+ghDefinition_path = os.path.join(dir_path, 'ghCompute.gh')
+
+rcutil.url = compute_url
+rcutil.authToken = "" # no auth token required as it is a local service
+
+# Test the compute server connection, should return version object
+print("T")
+try: 
+    version_test = requests.get(compute_url + '/version')
+    print("[INFO] Compute server connection established")
+except:
+    print("[ERROR] Can not establish a connection to the compute server!")
+    sys.exit(1)
+
+#Define the inputs:
+inputTree1 = rcgh.DataTree("RH_IN:number1")
+inputTree2 = rcgh.DataTree("number2")
+
+#Filling the trees:
+for i in range(1,6):   #1,2,3,4,5 
+    inputTree1.Append([i], [i]) #Beides müssen listen sein!
+
+inputTree2.Append([0], [3])
+
+# Merge all inputs:
+trees = [inputTree1, inputTree2]
+
+# Run the grasshopper script
+output = rcgh.EvaluateDefinition(ghDefinition_path, trees)
+
+# Process the formatted output:
+print(json.dumps(output,indent=4))
---
+++ flask_sample.py
@@ -0,0 +1,57 @@
+from flask import Flask
+import ghhops_server as hs
+import rhino3dm
+import os
+
+# Hops als Middleware registrieren
+app = Flask(__name__)
+hops: hs.HopsFlask = hs.Hops(app)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+# Definition von Component 1
+@hops.component(
+    "/add",
+    name="Add",
+    nickname="Add",
+    description="Add numbers with CPython",
+    icon = os.path.join(dir_path, "recources\\icon_add.png"),
+    inputs=[
+        hs.HopsNumber("A", "A", "First number"),
+        hs.HopsNumber("B", "B", "Second number"),
+        #hs.HopsNumber("B", "B", "Second number", hs.HopsParamAccess.LIST),
+    ],
+    outputs=[hs.HopsNumber("Sum", "S", "A + B")]
+)
+
+# Programm 1
+def add(a: float, b: float):
+    return a + b
+
+# Component 2
+@hops.component(
+    "/srf4pt",
+    name="4Point Surface",
+    nickname="Srf4Pt",
+    description="Create ruled surface from four points",
+    inputs=[
+        hs.HopsPoint("Corner A", "A", "First corner"),
+        hs.HopsPoint("Corner B", "B", "Second corner"),
+        hs.HopsPoint("Corner C", "C", "Third corner"),
+        hs.HopsPoint("Corner D", "D", "Fourth corner")
+    ],
+    outputs=[hs.HopsSurface("Surface", "S", "Resulting surface")]
+)
+# Funktion 2
+def ruled_surface(a: rhino3dm.Point3d,
+                  b: rhino3dm.Point3d,
+                  c: rhino3dm.Point3d,
+                  d: rhino3dm.Point3d):
+    edge1 = rhino3dm.LineCurve(a, b)
+    edge2 = rhino3dm.LineCurve(c, d)
+    return rhino3dm.NurbsSurface.CreateRuledSurface(edge1, edge2)
+
+# Starten des Flask servers
+if __name__ == "__main__":
+    app.run(host='localhost', port=6000,debug=True)
